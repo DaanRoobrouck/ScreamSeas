d44698436341ff1c1f7747427c93311dfac91ed3
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2020-08-31 21:58:35 +0200

    animation, level design, fps controller, menu

---
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float AmbientSoundVolume = 0.8f;
+    public float SfxVolume = 0.8f;
+
+    private static GameManager Instance;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- UnityProject/Assets/Scripts/Navigator.cs
+++ UnityProject/Assets/Scripts/Navigator.cs
@@ -1,18 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+using System;
 
 public class Navigator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _parentPanel;
+    [SerializeField] private GameObject _menuPanel;
+    [SerializeField] private GameObject _optionsPanel;
+
+    [SerializeField] private AudioMixer _ambientSoundMixer;
+    [SerializeField] private AudioMixer _soundEffectsMixer;
+
+    [SerializeField] private Slider _ambientSoundSlider;
+    [SerializeField] private Slider _soundEffectsSlider;
+
+    [SerializeField] private PlayableDirector _director;
+
+    private GameManager _gameManager;
+    private void Start()
     {
-        
+        _gameManager = (GameManager)FindObjectOfType(typeof(GameManager));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        _parentPanel.SetActive(false);
+        _director.Play();
+
+        StartCoroutine(LoadGame((float)_director.duration));
+    }
+
+    private IEnumerator LoadGame(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        _menuPanel.SetActive(false);
+        _optionsPanel.SetActive(true);
+
+        _ambientSoundSlider.value = _gameManager.AmbientSoundVolume;
+        _soundEffectsSlider.value = _gameManager.SfxVolume;
+    }
+
+    public void UpdateAmbientAudio()
+    {
+        float volume = Mathf.Log10(_ambientSoundSlider.value) * 20;
+        _ambientSoundMixer.SetFloat("AmbientSound", volume);
+        _gameManager.AmbientSoundVolume = _ambientSoundSlider.value;
+    }
+
+    public void UpdateSoundEffectsAudio()
+    {
+        float volume = Mathf.Log10(_soundEffectsSlider.value) * 20;
+        _soundEffectsMixer.SetFloat("SoundEffects", volume);
+        _gameManager.SfxVolume = _soundEffectsSlider.value;
+    }
+
+    public void Back()
+    {
+        _menuPanel.SetActive(true);
+        _optionsPanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
